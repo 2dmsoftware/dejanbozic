e5b4d5bbaacda386bcb77528da951212bc3b2726
2dmsoftware <2dmsoftware@gmail.com>
2025-04-02 10:37:54 +0200

    Initialize preload-milo

---
+++ scripts/scripts.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+// Add project-wide style path here.
+const STYLES = '';
+
+const decorateArea = (area = document) => {
+  // LCP image decoration
+  (function decorateLCPImage() {
+    const lcpImg = area.querySelector('img');
+    lcpImg?.removeAttribute('loading');
+  }());
+};
+
+// Add any config options.
+const CONFIG = {
+  // codeRoot: '',
+  // contentRoot: '',
+  // imsClientId: 'college',
+  // geoRouting: 'off',
+  // fallbackRouting: 'off',
+  decorateArea,
+  locales: {
+    '': { ietf: 'en-US', tk: 'hah7vzn.css' },
+    de: { ietf: 'de-DE', tk: 'hah7vzn.css' },
+    kr: { ietf: 'ko-KR', tk: 'zfo3ouc' },
+  },
+};
+
+// Decorate the page with site specific needs.
+decorateArea();
+
+/*
+ * ------------------------------------------------------------
+ * Edit below at your own risk
+ * ------------------------------------------------------------
+ */
+
+const { miloLibs } = window;
+
+(function loadStyles() {
+  const paths = [`${miloLibs}/styles/styles.css`];
+  if (STYLES) { paths.push(STYLES); }
+  paths.forEach((path) => {
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', path);
+    document.head.appendChild(link);
+  });
+}());
+
+(async function loadPage() {
+  const { loadArea, setConfig } = await import(`${miloLibs}/utils/utils.js`);
+  setConfig({ ...CONFIG, miloLibs });
+  await loadArea();
+}());
